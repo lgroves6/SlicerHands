dda3b8e677be33cabbaf2e11f829e148284db91d
Leah Groves <lgroves6@uwo.ca>
2019-08-01 14:48:36 -0400

    changes to sphere size

--- HandGenerator/HandGenerator.py
+++ HandGenerator/HandGenerator.py
@@ -96,7 +96,7 @@ class HandGeneratorWidget(ScriptedLoadableModuleWidget):
       for i in range (0, self.n):
         if 'Left' in self.nodes[i].GetName() or 'Right' in self.nodes[i].GetName():
           if 'Dis' in self.nodes[i].GetName() or 'Int' in self.nodes[i].GetName() or 'Prox' in self.nodes[i].GetName() or 'Meta' in self.nodes[i].GetName():
-            self.spheres = l.CreateSphere(3)
+            self.spheres = l.CreateSphere((float(self.nodes[i].GetAttribute('radiusMm'))/10)*2)
             self.spheres.SetAndObserveTransformNodeID(self.nodes[i].GetID())
             self.spheres.SetName('Sphere'+self.nodes[i].GetName())
             self.Zshift = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLLinearTransformNode')
@@ -105,7 +105,7 @@ class HandGeneratorWidget(ScriptedLoadableModuleWidget):
             mat.SetElement(2,3,self.length/2)
             self.Zshift.SetAndObserveMatrixTransformToParent(mat)
             self.Zshift.SetAndObserveTransformNodeID(self.nodes[i].GetID())
-            self.cyl = l.CreateCylinder(self.length-1.5, 1.5)
+            self.cyl = l.CreateCylinder(self.length-1.5,(float(self.nodes[i].GetAttribute('radiusMm'))/10))
             self.cyl.SetAndObserveTransformNodeID(self.Zshift.GetID())
             self.cyl.SetName('Cyl'+self.nodes[i].GetName())
     else:
@@ -115,18 +115,21 @@ class HandGeneratorWidget(ScriptedLoadableModuleWidget):
       mat = vtk.vtkMatrix4x4()
       l = slicer.modules.createmodels.logic()
       for j in range(0, len(self.models)): 
-        if 'Cyl' in self.models[j].GetName():
-          slicer.mrmlScene.RemoveNode(self.models[j])
+        slicer.mrmlScene.RemoveNode(self.models[j])
       for i in range (0, self.n):
         if 'Zshift' in self.nodes[i].GetName(): 
           slicer.mrmlScene.RemoveNode(self.nodes[i])
         if 'Left' in self.nodes[i].GetName() or 'Right' in self.nodes[i].GetName():
           if 'Dis' in self.nodes[i].GetName() or 'Int' in self.nodes[i].GetName() or 'Prox' in self.nodes[i].GetName() or 'Meta' in self.nodes[i].GetName():
+            self.spheres = l.CreateSphere((float(self.nodes[i].GetAttribute('radiusMm'))/10)*2)
+            self.spheres.SetAndObserveTransformNodeID(self.nodes[i].GetID())
+            self.spheres.SetName('Sphere'+self.nodes[i].GetName())
             self.Zshift = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLLinearTransformNode')
             self.Zshift.SetName('Zshift')
             self.length = float(self.nodes[i].GetAttribute('lengthMm'))
             mat.SetElement(2,3,self.length/2)
             self.Zshift.SetAndObserveMatrixTransformToParent(mat)
             self.Zshift.SetAndObserveTransformNodeID(self.nodes[i].GetID())
-            self.cyl = l.CreateCylinder(self.length-1.5, 1.5)
+            self.cyl = l.CreateCylinder(self.length-1.5,(float(self.nodes[i].GetAttribute('radiusMm'))/10))
             self.cyl.SetAndObserveTransformNodeID(self.Zshift.GetID())
+            self.cyl.SetName('Cyl'+self.nodes[i].GetName())
